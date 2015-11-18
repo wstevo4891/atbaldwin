a02997d5d924a63e2defad7c7a8917b3e407a8b4
Eric Stephenson <ericb.stephenson@gmail.com>
2015-11-18 15:46:20 -0800

    disabled asset precompile in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Mystore
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
+    
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
