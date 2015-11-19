9afdeccee4fb807e3685bfd06b23e22e963d8067
Eric Stephenson <ericb.stephenson@gmail.com>
2015-11-19 12:30:04 -0800

    configured action_mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = { :host => "https://vast-reef-1675.herokuapp.com" }
 end
