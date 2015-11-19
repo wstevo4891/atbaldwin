1a42ed440ac0ec9f0fee68dd937ad78aa9cd1744
Eric Stephenson <ericb.stephenson@gmail.com>
2015-11-19 12:34:49 -0800

    set action_mailer port to 25

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :port => 25
+  }
   
   config.action_mailer.default_url_options = { :host => "https://vast-reef-1675.herokuapp.com" }
 end
