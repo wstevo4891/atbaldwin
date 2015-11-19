ccb83f4f3ba5956d61ba2c81dfeb1a6a872c034c
Eric Stephenson <ericb.stephenson@gmail.com>
2015-11-19 13:34:32 -0800

    corrected parameters in SendGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app44049428@heroku.com'],
-    :password       => ENV['e0fkqnhj3447'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
